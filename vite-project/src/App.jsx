b2ec01c36f2735f2f2cf01c208de1dc8460d18b0
zak2721 <zekiostheone@gmail.com>
2024-12-10 04:39:36 -0800

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1> the vite document is the one who tawhat the world</h1>
+    </>
+  )
+}
+
+export default App
