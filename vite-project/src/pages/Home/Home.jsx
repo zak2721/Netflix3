d229f1eb153522e19718eb2a93cccad97b0c6973
zak2721 <zekiostheone@gmail.com>
2024-12-17 02:56:00 -0800

    added some  minor fixes in Home.jsx

--- vite-project/src/pages/Home/Home.jsx
+++ vite-project/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Footer/Footer'
 import Banner from '../../Components/Banner/Banner'
-import Rowlist from '../../Components/Rows/Rowlist/Rowlist '
+import Rowlist from '../../Components/Rows/Rowlist/Rowlist.jsx'
 
 
 const Home = () => {
